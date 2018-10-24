1214a898a7ef6c5c08313dbc989a5ed2408e542e
Tatsuya Asami <sharekobekko@gmail.com>
2018-10-24 22:34:07 +0900

    created prototype of Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,84 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import store from '../redux/store';
+import { addInfo } from '../redux/actions/action';
 
-class Form extends React.component {
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addInfo: info => dispatch(addInfo(info)),
+  };
+};
+
+class ConnectedForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Tom',
-      age: 42,
-      department: 'Marketing',
-      position: 'Manager',
-      join: 2015,
-      leave: 2017,
-      location: 'Tokyo',
+      name: '',
+      age: '',
     };
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleAgeChange = this.handleAgeChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  handleAgeChange(event) {
+    this.setState({
+      age: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { name, age } = this.state;
+    this.props.addInfo({ name, age });
+    store.getState();
+    this.setState({
+      name: '',
+      age: '',
+    });
   }
 
   render() {
+    const { name, age } = this.state;
+    console.log(name, age);
+
     return (
       <div>
-        <p>aaa</p>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="Name">
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleNameChange}
+            />
+          </label>
+          <label htmlFor="age">
+            age:
+            <input
+              type="text"
+              name="age"
+              value={age}
+              onChange={this.handleAgeChange}
+            />
+          </label>
+          <button type="submit" value="Send">
+            submit
+          </button>
+        </form>
       </div>
     );
   }
 }
+
+const Form = connect(null, mapDispatchToProps)(ConnectedForm);
 
 export default Form;
