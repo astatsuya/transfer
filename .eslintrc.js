0dea0eff415c53b9422cda227c0b37521d128e2e
Tatsuya Asami <sharekobekko@gmail.com>
2018-10-25 00:27:05 +0900

    destructing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "extends": "airbnb",
     "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "prefer-destructuring": ["error", {"object": false, "array": false}]
     },
 };
