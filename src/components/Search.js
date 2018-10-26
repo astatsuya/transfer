3dc9531114755e59d9340605ad877ced393254c4
Tatsuya Asami <sharekobekko@gmail.com>
2018-10-26 19:23:41 +0900

    connected to redux

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,20 +4,32 @@ import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
   return {
+    columns: state.columns,
     info: state.info,
   };
 };
 
-const ConnectedSearch = ({ info }) => (
+const ConnectedSearch = ({ columns, info }) => (
   <table>
     <tr>
-      <th>name</th>
-      <th>age</th>
-      </tr>
-    <tr>
-      <td>{info[0].name}</td>
-      <td>{info[0].age}</td>
+      {columns.map(state => (
+        <th>{state}</th>
+      ))}
     </tr>
+    {info.map((state, index) => {
+      return (
+        <tr>
+          <td>{state.name}</td>
+          <td>{state.age}</td>
+          <td>{state.gender}</td>
+          <td>{state.department}</td>
+          <td>{state.position}</td>
+          <td>{state.arrival}</td>
+          <td>{state.leave}</td>
+          <td>{state.location}</td>
+        </tr>
+      );
+    })}
   </table>
 );
 
