ed36a8b2d22be2bb12eed3ad4d62decffb9c0595
Tatsuya Asami <sharekobekko@gmail.com>
2018-11-01 19:07:06 +0900

    revise eslint-rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
-    "extends": "airbnb",
-    "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+  "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     },
 };
