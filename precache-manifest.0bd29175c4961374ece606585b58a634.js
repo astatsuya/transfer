1298756716dc41945734476b918092d86ded83e1
Tatsuya Asami <sharekobekko@gmail.com>
2018-11-01 19:11:50 +0900

    Updates

---
+++ precache-manifest.0bd29175c4961374ece606585b58a634.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b54a4fc0a547bccd4c5",
+    "url": "/transfer/static/js/runtime~main.1b54a4fc.js"
+  },
+  {
+    "revision": "f522e5eccbe2eb508487",
+    "url": "/transfer/static/js/main.f522e5ec.chunk.js"
+  },
+  {
+    "revision": "ea3a7f6a3cc834a88af6",
+    "url": "/transfer/static/js/1.ea3a7f6a.chunk.js"
+  },
+  {
+    "revision": "f522e5eccbe2eb508487",
+    "url": "/transfer/static/css/main.6c250139.chunk.css"
+  },
+  {
+    "revision": "5fafbbc79362b0622c285cc04ce685d6",
+    "url": "/transfer/index.html"
+  }
+];
