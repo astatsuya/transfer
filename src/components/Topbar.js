55f58180032c3da445e672f359e8bc30ede08fb2
Tatsuya Asami <sharekobekko@gmail.com>
2018-11-13 15:47:17 +0900

    revised how to write

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,22 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LinkButton from './LinkButton';
-import { Link } from 'react-router-dom'
-
 
 const Topbar = () => (
   <div className="topbar">
     <h3>社員データ</h3>
-    <Link to={process.env.PUBLIC_URL + '/'}>
-      <LinkButton buttonName={'トップ'}  />
+    <Link to={`${process.env.PUBLIC_URL}/`}>
+      <LinkButton buttonName="トップ" />
     </Link>
     <Link to="/form/">
-      <LinkButton buttonName={'情報入力'} />
+      <LinkButton buttonName="情報入力" />
     </Link>
     <Link to="/search/">
-      <LinkButton buttonName={'情報検索'} />
+      <LinkButton buttonName="情報検索" />
     </Link>
     <Link to="/database/">
-      <LinkButton buttonName={'データベース'} />
+      <LinkButton buttonName="登録データ" />
     </Link>
   </div>
 );
