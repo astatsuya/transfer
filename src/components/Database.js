00e644337d9692f08de0f47e18f9c1c0049d2ddc
Tatsuya Asami <sharekobekko@gmail.com>
2018-11-13 21:44:30 +0900

    changed key and add propTypes

--- src/components/Database.js
+++ src/components/Database.js
@@ -1,48 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { addInfo } from '../redux/actions/action';
+import PropTypes from 'prop-types';
 
-const mapStateToProps = state => {
-  return {
-    columns: state.columns,
-    info: state.info,
-  };
-};
+// import { addInfo } from '../redux/actions/action';
 
-const mapDispatchToProps = dispatch => {
-  return {
-    addInfo: info => dispatch(addInfo(info)),
-  };
-};
+const mapStateToProps = state => ({
+  columns: state.columns,
+  info: state.info,
+});
 
-const ConnectedDataBase = ({ dispatch, columns, id, info }) => {
-  return (
-  <table className='database'>
+// const mapDispatchToProps = dispatch => ({
+//   addInfo: info => dispatch(addInfo(info)),
+// });
+
+const ConnectedDataBase = ({
+  columns,
+  info,
+}) => (
+  <table className="database">
     <tbody>
-      <tr key='columnstr'>
-        {columns.map((state, index) => (
-          <th key={`columnsth${index}`}>{state}</th>
+      <tr key="columnstr">
+        {columns.map(state => (
+          <th key={`columnsth${state}`}>{state}</th>
         ))}
       </tr>
-      {info.map((state, index) => {
-        return (
-          <tr key={`tr${index}`}>
-            <td key={`name${index}`}>{state.name}</td>
-            <td key={`age${index}`}>{state.age}</td>
-            <td key={`gender${index}`}>{state.gender}</td>
-            <td key={`department${index}`}>{state.department}</td>
-            <td key={`position${index}`}>{state.position}</td>
-            <td key={`arrival${index}`}>{state.arrival}</td>
-            <td key={`leave${index}`}>{state.leave}</td>
-            <td key={`location${index}`}>{state.location}</td>
-          </tr>
-        );
-      })}
+      {info.map(state => (
+        <tr key={`tr${state.id}`}>
+          <td key={`name${state.id}`}>{state.name}</td>
+          <td key={`age${state.id}`}>{state.age}</td>
+          <td key={`gender${state.id}`}>{state.gender}</td>
+          <td key={`department${state.id}`}>{state.department}</td>
+          <td key={`position${state.id}`}>{state.position}</td>
+          <td key={`arrival${state.id}`}>{state.arrival}</td>
+          <td key={`leave${state.id}`}>{state.leave}</td>
+          <td key={`location${state.id}`}>{state.location}</td>
+        </tr>
+      ))}
     </tbody>
   </table>
-  );
+);
+
+ConnectedDataBase.propTypes = {
+  columns: PropTypes.arrayOf(PropTypes.string).isRequired,
+  info: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-const DataBase = connect(mapStateToProps, mapDispatchToProps)(ConnectedDataBase);
+const DataBase = connect(mapStateToProps)(ConnectedDataBase);
 
 export default DataBase;
