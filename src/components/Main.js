4db614b0e51d81113f06cf7fe8cb032d37b1b2eb
Tatsuya Asami <sharekobekko@gmail.com>
2018-11-13 15:48:57 +0900

    follow ESlint

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,15 @@
+/* global App, PageContents */
+
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-const Main = () => {
+const Main = () => (
   <main>
     <Switch>
-      <Route exact path='/' component={App} />
-      <Route path='/pagecontents' component={PageContents} />
+      <Route exact path="/" component={App} />
+      <Route path="/pagecontents" component={PageContents} />
     </Switch>
   </main>
-}
+);
 
 export default Main;
