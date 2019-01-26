91f469ad62d3a9505b9ad13fdb724c65c2a506d3
Tatsuya Asami <sharekobekko@gmail.com>
2019-01-26 19:27:20 +0900

    revised style for following ESling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Form from './components/Form';
 import DataBase from './components/Database';
 import SearchForm from './components/Searchform';
 import PageContents from './components/PageContents';
 import Topbar from './components/Topbar';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom'
+
 
 const App = () => (
   <BrowserRouter>
     <div className="main-page">
       <Topbar />
 
-      <Route path={process.env.PUBLIC_URL + '/'} exact component={PageContents} />
+      <Route path={`${process.env.PUBLIC_URL}/`} exact component={PageContents} />
       <Route path="/form" component={Form} />
       <Route path="/search" component={SearchForm} />
       <Route path="/database" component={DataBase} />
