d9ba2229031b3fd11e36e0b7dd034bf41cc32816
Tatsuya Asami <sharekobekko@gmail.com>
2019-01-27 14:27:52 +0900

    divided mapStateToProps, mapDispatchToProps, and sortableTalbes

--- src/components/MapToProps.js
+++ src/components/MapToProps.js
@@ -3,18 +3,169 @@ import {
   updateForm,
   clearForm,
   alertForm,
+  sortTable,
 } from '../redux/actions/action';
 
+export const sortTableState = (state, sortBy) => {
+  switch (sortBy.sortCase) {
+    case 'ID':
+      if (sortBy.order === 'asc') {
+        return state.sort((a, b) => a.id - b.id);
+      }
+      return state.sort((a, b) => b.id - a.id);
+    case 'Name':
+      return state.sort((a, b) => {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
+        if (sortBy.order === 'asc') {
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        }
+        if (sortBy.order === 'desc') {
+          if (nameA > nameB) {
+            return -1;
+          }
+          if (nameA < nameB) {
+            return 1;
+          }
+          return 0;
+        }
+        return 0;
+      });
+    case 'Age':
+      if (sortBy.order === 'asc') {
+        return state.sort((a, b) => a.age - b.age);
+      }
+      return state.sort((a, b) => b.age - a.age);
+    case 'Gender':
+      return state.sort((a, b) => {
+        const nameA = a.gender.toUpperCase();
+        const nameB = b.gender.toUpperCase();
+        if (sortBy.order === 'asc') {
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        }
+        if (sortBy.order === 'desc') {
+          if (nameA > nameB) {
+            return -1;
+          }
+          if (nameA < nameB) {
+            return 1;
+          }
+          return 0;
+        }
+        return 0;
+      });
+    case 'Department':
+      return state.sort((a, b) => {
+        const nameA = a.department.toUpperCase();
+        const nameB = b.department.toUpperCase();
+        if (sortBy.order === 'asc') {
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        }
+        if (sortBy.order === 'desc') {
+          if (nameA > nameB) {
+            return -1;
+          }
+          if (nameA < nameB) {
+            return 1;
+          }
+          return 0;
+        }
+        return 0;
+      });
+    case 'Position':
+      return state.sort((a, b) => {
+        const nameA = a.position.toUpperCase();
+        const nameB = b.position.toUpperCase();
+        if (sortBy.order === 'asc') {
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        }
+        if (sortBy.order === 'desc') {
+          if (nameA > nameB) {
+            return -1;
+          }
+          if (nameA < nameB) {
+            return 1;
+          }
+          return 0;
+        }
+        return 0;
+      });
+    case 'Arrival':
+      if (sortBy.order === 'asc') {
+        return state.sort((a, b) => a.arrival - b.arrival);
+      }
+      return state.sort((a, b) => b.arrival - a.arrival);
+    case 'Leave':
+      if (sortBy.order === 'asc') {
+        return state.sort((a, b) => a.leave - b.leave);
+      }
+      return state.sort((a, b) => b.leave - a.leave);
+    case 'Location':
+      return state.sort((a, b) => {
+        const nameA = a.location.toUpperCase();
+        const nameB = b.location.toUpperCase();
+        if (sortBy.order === 'asc') {
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        }
+        if (sortBy.order === 'desc') {
+          if (nameA > nameB) {
+            return -1;
+          }
+          if (nameA < nameB) {
+            return 1;
+          }
+          return 0;
+        }
+        return 0;
+      });
+    default:
+      return state;
+  }
+};
 
 export const mapDispatchToProps = dispatch => ({
   addInfo: info => dispatch(addInfo(info)),
   updateForm: form => dispatch(updateForm(form)),
   alertForm: form => dispatch(alertForm(form)),
   clearForm: () => dispatch(clearForm()),
+  sortTable: (sortCase, order) => dispatch(sortTable(sortCase, order)),
 });
 
 export const mapStateToProps = state => ({
   formContents: state.updateForm.formContents,
+  columns: state.addInfo.columns,
+  info: sortTableState(state.addInfo.info, state.sortTable),
+  state,
 });
 
 const PropTypes = 'PropTypes';
