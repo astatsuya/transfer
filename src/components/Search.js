6a7c48c5262452442ba354e12e37a71f3fb77804
Tatsuya Asami <sharekobekko@gmail.com>
2019-02-03 00:10:09 +0900

    fixed bug for termStayFiltered and termStayAll

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,48 +8,43 @@ const ConnectedSearch = ({ info, searchForm }) => {
 
   const length = a => a.reduce(() => a.length, 0);
 
-  // 男性か女性か
-  const selectedGender = searchForm.gender;
+  const {
+    gender,
+    department,
+    position,
+    location,
+  } = searchForm;
 
-  const filterGender = info.filter((value) => {
-    if (selectedGender !== 'all') {
-      return value.gender === selectedGender;
+  // トグル選択で選択したのと一致する項目のみ抽出
+  const filteredInfo = info.filter((value, index, array) => {
+    if (gender !== 'all') {
+      return value.gender === gender;
     }
-    return value.gender;
-  });
-  // その中で部署はどこか
-  const selectedDepartment = searchForm.department;
+    return array;
+  })
+    .filter((value, index, array) => {
+      if (department !== 'all') {
+        return value.department === department;
+      }
+      return array;
+    })
+    .filter((value, index, array) => {
+      if (position !== 'all') {
+        return value.position === position;
+      }
+      return array;
+    })
+    .filter((value, index, array) => {
+      if (location !== 'all') {
+        return value.location === location;
+      }
+      return array;
+    });
 
-  const filterDepartment = filterGender.filter((value) => {
-    if (selectedDepartment !== 'all') {
-      return value.department === selectedDepartment;
-    }
-    return value.department;
-  });
-  // その中でポジションは何か
-  const selectedPosition = searchForm.position;
-
-  const filterPosition = filterDepartment.filter((value) => {
-    if (selectedPosition !== 'all') {
-      return value.position === selectedPosition;
-    }
-    return value.position;
-  });
-  // その中でロケーションはどこか
-  const selectedLocation = searchForm.location;
-
-  const filterLocation = filterPosition.filter((value) => {
-    if (selectedLocation !== 'all') {
-      return value.location === selectedLocation;
-    }
-    return value.location;
-  });
-
-  // 選択した部署のLeave - Arrival
-  const termFilterArray = filterPosition.map(a => a.leave - a.arrival);
+  const termFilterArray = filteredInfo.map(a => a.leave - a.arrival);
 
   // 選択した部署の配列のposition要素の羅列
-  const lengthFilterArrayy = filterPosition.map(a => a.position);
+  const lengthFilterArrayy = filteredInfo.map(a => a.position);
 
   // 全員のLeave - arrivalの列挙
   const termStayEach = info.map(a => a.leave - a.arrival);
@@ -61,7 +56,7 @@ const ConnectedSearch = ({ info, searchForm }) => {
   const lengthArrayAll = length(nameAll);
   // 全員の平均勤続年数 = (全員のleave-arrivalの合計) / 該全員の配列の要素数
   const termStayAverage = (sum(termStayEach) / lengthArrayAll)
-  === true ? (sum(termStayEach) / lengthArrayAll).toFixed(1) : 0;
+    ? (sum(termStayEach) / lengthArrayAll).toFixed(1) : 0;
   // 全員の異動先毎の人数
   const newLocationAll = () => {
     const lengthOfTokyo = locationAll.filter(array => array === 'Tokyo').length;
@@ -80,10 +75,10 @@ const ConnectedSearch = ({ info, searchForm }) => {
   const lengthArrayFiltered = length(lengthFilterArrayy);
   // 該当者の平均勤続年数 = (該当者のleave-arrivalの合計) / 該当者の配列の要素数
   const termStayFiltered = (sum(termFilterArray) / lengthArrayFiltered)
-  === true ? (sum(termFilterArray) / lengthArrayFiltered).toFixed(1) : 0;
+    ? (sum(termFilterArray) / lengthArrayFiltered).toFixed(1) : 0;
 
   // 該当者の異動先の列挙
-  const newLocation = filterLocation.map(a => a.location);
+  const newLocation = filteredInfo.map(a => a.location);
   // 該当者の異動先毎の人数
   const newLocationFiltered = () => {
     const lengthOfTokyo = newLocation.filter(array => array === 'Tokyo').length;
